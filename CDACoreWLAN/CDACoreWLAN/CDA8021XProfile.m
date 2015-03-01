7e403753444d66acd89127605d33d91e09072713
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-02-28 20:22:09 -0500

    added project

---
+++ CDACoreWLAN/CDACoreWLAN/CDA8021XProfile.m
@@ -0,0 +1,13 @@
+//
+//  CDA8021XProfile.m
+//  CDACoreWLAN
+//
+//  Created by Alsey Coleman Miller on 2/28/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+#import "CDA8021XProfile.h"
+
+@implementation CDA8021XProfile
+
+@end
---
+++ CDACoreWLAN/CDACoreWLAN/CDACoreWLAN.h
@@ -0,0 +1,17 @@
+//
+//  CDACoreWLAN.h
+//  CDACoreWLAN
+//
+//  Created by Alsey Coleman Miller on 2/28/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+#import <ObjFW/ObjFW.h>
+
+//! Project version number for CDACoreWLAN.
+FOUNDATION_EXPORT double CDACoreWLANVersionNumber;
+
+//! Project version string for CDACoreWLAN.
+FOUNDATION_EXPORT const unsigned char CDACoreWLANVersionString[];
+
+#import <CDACoreWLAN/CDA8021XProfile.h>
